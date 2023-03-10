302e797be7d7310f2ac1d1f099b12614bed76448
Кандрушин Максим 22-IST-1 <125025831+heiby-baby@users.noreply.github.com>
2023-03-10 23:04:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,43 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int cbinsearch_Helper_left(int* arr, int size, int value) {
+  int left = 0;
+  int right = size - 1;
+  while (left <= right) {
+    int	mid = (left + right) / 2;
+    if (arr[mid] == value && mid == 0) return 0;
+    if ((arr[mid] == value && arr[mid - 1] != value)) return mid;
+    if (value > arr[mid]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+
+int cbinsearch_Helper_right(int* arr, int size, int value) {
+  int left = 0;
+  int right = size - 1;
+  if (arr[right] == value) return right;
+  while (left <= right) {
+    int mid = (left + right) / 2;
+    if (arr[mid] == value && mid == 0) return 0;
+    if ((arr[mid] == value && arr[mid + 1] != value)) return mid;
+    if (value < arr[mid]) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+		}
+  }
+  return -1;
+}
+
+int cbinsearch(int* arr, int size, int value) {
+  if (cbinsearch_Helper_left(arr, size, value) == -1) {
+    return 0;
+  }
+  if (arr[cbinsearch_Helper_left(arr, size, value)] == arr[cbinsearch_Helper_right(arr, size, value)]) {
+    return (cbinsearch_Helper_right(arr, size, value) - cbinsearch_Helper_left(arr, size, value)) + 1;
+  }
 }
